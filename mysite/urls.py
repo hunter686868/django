5d0302cb1450e084b49c839dfbbc3fde49910ec4
Sergei <hunter6868@ya.ru>
2024-08-09 00:08:34 +0300

    update

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,12 +20,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("shop/", include('site_shop.urls')),
     path("", RedirectView.as_view(url='/shop/', permanent=True)),
 ]
 
-urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns + static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
+# Correct way to append static and media URLs
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- site_shop/models.py
+++ site_shop/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Section(models.Model):
+    title = models.CharField(
+        max_length=100,
+        help_text='Введите название раздела',
+        unique=True,
+        verbose_name='Название раздела'
+    )
+
+    class Meta:
+        ordering = ['id'],
+        verbose_name = 'Раздел',
+        verbose_name_plural = 'Разделы'
+
+    def __str__(self):
+        return self.title
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Photo(models.Model):
+    album = models.ForeignKey(Album, related_name='photos', on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='photos/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
