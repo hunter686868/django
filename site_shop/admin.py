6add143d6d588283718bf66404613e2b893089e0
hunter <hunter6868@ya.ru>
2024-08-11 18:13:03 +0300

    update

--- site_shop/admin.py
+++ site_shop/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from site_shop.models import Section, Product, Discount, Order, OrderLine
+from site_shop.models import Section, Product, Discount, Order, OrderLine, Album, Photo, Article, Comment, \
+    FeedbackMessage
 
 # Register your models here.
 admin.site.register(Section)
@@ -8,3 +9,8 @@ admin.site.register(Product)
 admin.site.register(Discount)
 admin.site.register(Order)
 admin.site.register(OrderLine)
+admin.site.register(Album)
+admin.site.register(Photo)
+admin.site.register(Article)
+admin.site.register(Comment)
+admin.site.register(FeedbackMessage)
