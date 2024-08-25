7a5f13642ec75c31123cf7a33ec3b3d3a331648a
Sergei <hunter6868@ya.ru>
2024-08-25 14:23:54 +0300

    update

--- site_shop/views.py
+++ site_shop/views.py
@@ -1,15 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from site_shop.models import Section
+from site_shop.models import Section, Photo
 
 
 def index(request):
     sections = Section.objects.all().order_by('title')
+    photos = Photo.objects.all()[:12]
     return render(
         request,
         'index.html',
-        context={'sections': sections}
+        context={'sections': sections,
+                 'photos': photos}
     )
 
 
